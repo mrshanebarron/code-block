9973bbb05462d3204c67b982b0995861c525e678
Shane Barron <shane@Mac.hsd1.fl.comcast.net>
2025-12-15 18:58:05 -0500

    Fix PHP syntax - use PascalCase for all class names

--- src/CodeBlockServiceProvider.php
+++ src/CodeBlockServiceProvider.php
@@ -3,14 +3,36 @@
 namespace MrShaneBarron\CodeBlock;
 
 use Illuminate\Support\ServiceProvider;
+use MrShaneBarron\CodeBlock\Livewire\CodeBlock;
+use MrShaneBarron\CodeBlock\View\Components\CodeBlock as BladeCodeBlock;
 
 class CodeBlockServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/sb-code-block.php', 'sb-code-block');
+    }
+
     public function boot(): void
     {
-        if (class_exists(\Livewire\Livewire::class)) {
-            \Livewire\Livewire::component('sb-code-block', Livewire\CodeBlock::class);
-        }
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sb-code-block');
+
+        if (class_exists(\Livewire\Livewire::class)) {
+            \Livewire\Livewire::component('sb-code-block', CodeBlock::class);
+        }
+
+        $this->loadViewComponentsAs('ld', [
+            BladeCodeBlock::class,
+        ]);
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/sb-code-block.php' => config_path('sb-code-block.php'),
+            ], 'sb-code-block-config');
+
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/sb-code-block'),
+            ], 'sb-code-block-views');
+        }
     }
 }
